b743eab6fa4cb7d6237be1f746a0325a268787bb
akm <akm2000@gmail.com>
2019-05-07 18:57:43 +0900

    :recycle: Extract AbstractBuilder

--- lib/goon_model_gen/builder/model_builder.rb
+++ lib/goon_model_gen/builder/model_builder.rb
@@ -1,10 +1,10 @@
 require "goon_model_gen"
 
-require "goon_model_gen/source/context"
+require "goon_model_gen/builder/abstract_builder"
+
 require "goon_model_gen/source/struct"
 require "goon_model_gen/source/enum"
 require "goon_model_gen/source/named_slice"
-
 
 require "goon_model_gen/golang/package"
 require "goon_model_gen/golang/packages"
@@ -14,29 +14,7 @@ require "active_support/core_ext/string"
 
 module GoonModelGen
   module Builder
-    class ModelBuilder
-      attr_reader :base_package_path
-
-      def initialize(base_package_path)
-        @base_package_path = base_package_path
-      end
-
-
-      # @param context [Source::Context]
-      # @return [Golang::Packages]
-      def build(context)
-        Golang::Packages.new.tap do |r|
-          build_sentences = []
-          context.files.each do |f|
-            package_path = File.join(base_package_path, f.basename)
-            pkg, procs = build_package(package_path, f.types)
-            r << pkg
-            build_sentences.concat(procs)
-          end
-          r.resolve_type_names(Golang::DatastoreSupported.packages)
-          build_sentences.each(&:call)
-        end
-      end
+    class ModelBuilder < AbstractBuilder
 
       # @param package_path [String]
       # @param types [Array<Source::Type>]
@@ -64,6 +42,11 @@ module GoonModelGen
         return pkg, procs
       end
 
+      # @param pkgs [Golang::Packages]
+      def resolve_type_names(pkgs)
+        pkgs.resolve_type_names(Golang::DatastoreSupported.packages)
+      end
+
       # @param t [Source::Struct]
       # @param pkg [Golang::Package]
       # @return [Golang::Struct]
@@ -81,56 +64,6 @@ module GoonModelGen
           t.fields.each do |f|
             s.new_field(f.name, f.type_name, f.build_tags)
           end
-        end
-      end
-
-
-      # @param template_base [string]
-      # @param t [Source::Struct]
-      # @param go_type [Golang::Type]
-      def build_sentences(template_base, t, go_type)
-        m2t = method_to_template_for(template_base)
-        t.generators ||= default_generators_for(template_base)
-        t.generators.each do |name, suffix|
-          next if !suffix
-          template = m2t[name]
-          parts = [t.name.underscore]
-          custom_suffix = false
-          if suffix.is_a?(String)
-            parts << suffix
-            custom_suffix = true
-          end
-          filename = parts.join('_') << '.go'
-          go_type.package.find_or_new_file(filename).tap do |file|
-            file.custom_suffix = custom_suffix
-            file.new_sentence(File.join(template_base, template), go_type)
-          end
-        end
-      end
-
-      def default_generators_for(template_base)
-        method_to_template_for(template_base).keys.
-          each_with_object({}){|name, d| d[name] = true }
-      end
-
-      def method_to_template_map
-        @method_to_template_map ||= {}
-      end
-
-      def method_to_template_for(template_base)
-        method_to_template_map[template_base] ||=
-          begin
-            templates_for(template_base).each_with_object({}) do |filename, d|
-              m = filename.sub(/\A\d+\_/, '').sub(/\.go\.erb\z/, '')
-              d[m] = filename
-            end
-          end
-      end
-
-      def templates_for(template_base)
-        base_dir = File.join(File.expand_path('../../templates', __FILE__), template_base)
-        Dir.chdir(base_dir) do
-          Dir.glob('*.go.erb')
         end
       end
 
